2694c43319377c8324750c438b602d538ca12d14
Annoying-Kiddd <annoying.kid08@hotmail.com>
2026-02-25 14:20:11 +0000

    Godot project 1.0
    Ver1
    player model functioning, borrowed asset for it from:
    https://michaelgames.itch.io/2d-action-adventure-rpg-assets

---
+++ fmp-gardener/Player/Scripts/state_idle.gd
@@ -0,0 +1,27 @@
+class_name State_Idle extends State
+
+@onready var walk: Node = $"../Walk"
+
+##what happens when player enters state?
+func Enter() -> void:
+	player.UpdateAnimation("idle")
+	pass
+
+##what happens when player exits state?
+func Exit() -> void:
+	pass
+
+##what happens during _process update in this state?
+func Process( _delta : float ) -> State:
+	if player.direction != Vector2.ZERO:
+		return walk
+	player.velocity = Vector2.ZERO
+	return null
+
+##what happens during the _physics_process update in this state?
+func Physics( _delta : float ) -> State:
+	return null
+
+##what happens with input events in this state?
+func HandleInput( _event: InputEvent ) -> State:
+	return null
---
+++ fmp-gardener/Player/Scripts/state_walk.gd
@@ -0,0 +1,35 @@
+class_name State_Walk extends State
+
+@export var move_speed : float = 100.0
+
+@onready var idle: State_Idle = $"../Idle"
+
+
+##what happens when player enters state?
+func Enter() -> void:
+	player.UpdateAnimation("walk")
+	pass
+
+##what happens when player exits state?
+func Exit() -> void:
+	pass
+
+##what happens during _process update in this state?
+func Process( _delta : float ) -> State:
+	if player.direction == Vector2.ZERO:
+		return idle
+	
+	player.velocity = player.direction * move_speed
+	
+	if player.SetDirection():
+		player.UpdateAnimation("walk")
+	
+	return null
+
+##what happens during the _physics_process update in this state?
+func Physics( _delta : float ) -> State:
+	return null
+
+##what happens with input events in this state?
+func HandleInput( _event: InputEvent ) -> State:
+	return null
